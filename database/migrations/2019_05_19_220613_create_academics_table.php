790b13d009c1a1e70c5c9935cd01acdc1af695eb
AlvaroCC96 <alvaro.castillo@alumnos.ucn.cl>
2019-05-19 19:56:54 -0400

    Migraciones, tabla types y academic , modificacion de appservice

---
+++ database/migrations/2019_05_19_220613_create_academics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAcademicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('academics', function (Blueprint $table) {
+            $table->increments('id');
+
+            //los 3 son atributos del modelo R
+            $table->string('rut',128)->unique();
+            $table->string('name',128)->unique();
+            $table->string('email',128)->unique();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('academics');
+    }
+}
---
+++ database/migrations/2019_05_19_221344_create_types_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('types', function (Blueprint $table) {
+            $table->increments('id');
+
+            //aqui creamos los 4 campos de la tabla
+            $table->string('activity_name',128)->unique();
+            $table->string('students_number',128)->unique();
+            $table->integer('duration')->unsigned();
+            $table->string('req_external_org',128);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('types');
+    }
+}
