7bfa8075b98e160139412f50eaff5448419dca2d
AlvaroCC96 <alvaro.castillo@alumnos.ucn.cl>
2019-05-19 23:38:16 -0400

    creacion de tablas y relacionamientos

--- database/migrations/2019_05_20_220843_create_students_table.php
+++ database/migrations/2019_05_20_220843_create_students_table.php
@@ -19,11 +19,14 @@ class CreateStudentsTable extends Migration
             $table->string('rut',128)->notnullable();
             $table->string('name',128)->notnullable();
             $table->string('career',128)->notnullable(); //carrera
+            $table->string('email',128)->notnullable();
+            $table->integer('phone');
+            $table->integer('work_id')->notnullable();
             
-            
-
-
             $table->timestamps();
+
+            //relacionamiento con tabla work
+            $table->foreign('work_id')->references('id')->on('works')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
