88c20286e30d3a63be2b335c179688660eee8e03
AlvaroCC96 <alvaro.castillo@alumnos.ucn.cl>
2019-06-19 18:12:42 -0400

    eliminacion funciones que no se usan

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -79,7 +79,7 @@ class StudentController extends Controller
         if ($career == "Ingeniería Civil en Computación e Informática (Malla-Nueva)"){
             $student->careers()->attach(3);
         }
-        
+
         if ($career == "Ingeniería Civil en Computación e Informática (Malla-Antigua)"){
             $student->careers()->attach(4);
         }
@@ -153,85 +153,6 @@ class StudentController extends Controller
     }
 
 
-    public function check($rut) {
-
-        $cleanedRut = $this->clean($rut);
-
-        if (! $cleanedRut)
-            return false;
-
-        list($numero, $digitoVerificador) = explode('-', $cleanedRut);
-
-        //Validamos requisitos m?nimos
-        if ((($digitoVerificador != 'K') && (! is_numeric($digitoVerificador))) || (count(str_split($numero)) > 11))
-            return false;
-
-        //Validamos que todos los caracteres del n?mero sean d?gitos
-        foreach(str_split($numero) as $chr) {
-            if (! is_numeric($chr))
-                return false;
-        }
-
-        //Calculamos el digito verificador
-        $digit = $this->digitoVerificador($numero);
-
-        //Comparamos el digito verificador calculado con el entregado
-        if($digit == $digitoVerificador)
-            return true;
-
-        return false;
-    }
-
-    public function clean($originalRut, $incluyeDigitoVerificador = true) {
-
-        //Eliminamos espacios al principio y final
-        $originalRut = trim($originalRut);
-        //En caso de existir, eliminamos ceros ("0") a la izquierda
-        $originalRut = ltrim($originalRut, '0');
-        $input		= str_split($originalRut);
-        $cleanedRut	= '';
-        foreach ($input as $key => $chr) {
-            //Digito Verificador
-            if ((($key + 1) == count($input)) && ($incluyeDigitoVerificador)){
-                if (is_numeric($chr) || ($chr == 'k') || ($chr == 'K'))
-                    $cleanedRut .= '-'.strtoupper($chr);
-                else
-                    return false;
-            }
-            //N?meros del RUT
-            elseif (is_numeric($chr))
-                $cleanedRut .= $chr;
-        }
-
-        if (strlen($cleanedRut) < 3)
-            return false;
-
-        return $cleanedRut;
-    }
-
-    public function digitoVerificador($rut) {
-        //Preparamos el RUT recibido
-        $numero = $this->clean($rut, false);
-        //Calculamos el d?gito verificador
-        $txt		= array_reverse(str_split($numero));
-        $sum		= 0;
-        $factors	= array(2,3,4,5,6,7,2,3,4,5,6,7);
-        foreach($txt as $key => $chr) {
-            $sum += $chr * $factors[$key];
-        }
-
-        $a			= $sum % 11;
-        $b			= 11-$a;
-
-        if($b == 11)
-            $digitoVerificador	= 0;
-        elseif($b == 10)
-            $digitoVerificador	= 'K';
-        else
-            $digitoVerificador = $b;
-        //Convertimos el n?mero a cadena para efectos de poder comparar
-        $digitoVerificador = (string)$digitoVerificador;
-        return $digitoVerificador;
-    }
+    
 
 }
