7d30c1a25799c316d95c8ba21d64910eaab84e28
gnza928 <50747609+gnza928@users.noreply.github.com>
2019-07-02 03:56:00 -0400

    registrar examen de título

---
+++ resources/views/admin/works3/partials/form.blade.php
@@ -0,0 +1,36 @@
+
+<div class="form-group">
+{{ Form::label('grade','Nota')}}
+{{ Form::number('grade',null, ['class'=>'form-control','id' =>'grade','step'=>'any']) }}
+
+{{ Form::label('graduation_date','Fecha examen de título')}}
+{{ Form::date('graduation_date',null, ['class'=>'form-control','id' =>'graduation_date']) }}
+
+{{ Form::label('title','Título De La Actividad De Titulación')}}
+{{  Form::text('title',null, ['class'=>'form-control','id' =>'title','readonly'=>'true',]) }}
+
+{{ Form::label('status','Estado De La Actividad')}}
+{{ Form::text('status',null, ['class'=>'form-control','id' =>'status','readonly' => 'true']) }}
+
+{{ Form::label('start_date','Fecha programada de inicio')}}
+{{Form::date('start_date',null,['class'=>'form-control','id' =>'start_date','readonly' => 'true'])}}
+
+{{ Form::label('finish_date','Fecha programada de termino')}}
+{{Form::date('finish_date',null,['class'=>'form-control','id' =>'finish_date','readonly' => 'true'])}}
+
+{{ Form::label('year_reg','Año Registro: ')}}
+{{ Form::text('year_reg',null,['class'=>'form-control','id'=>'year_reg','readonly' => 'true']) }}
+
+{{ Form::label('cant_students','Cantidad De Estudiantes')}}
+{{ Form::text('cant_students',null, ['readonly'=>'true','class'=>'form-control','id' =>'cant_students']) }}
+
+{{ Form::label('semester_reg','Semestre Registro: ')}}
+{{ Form::text('semester_reg',null,['class'=>'form-control','id'=>'semester_reg','readonly' => 'true']) }}
+
+</div>
+
+<div class="form-group text-center">
+  {{
+    Form::submit('Registrar Examen',['class'=>'btn btn-success'])
+  }}
+</div>
